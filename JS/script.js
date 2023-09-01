474e6a7e47929145f286905f55948dfba09de1a9
ohidursohag <ohidursohag9595@gmail.com>
2023-09-01 10:44:32 +0600

    Cards dynamic update

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,23 @@
 // Show items on load
 window.onload = () => {
-   getCategories()
-   getCategorydata('1000')
+   getCategories();
+   getCategorydata('1000');
+   
 }
+
 
 // fetch categoris from api
 const getCategories = async () => {
    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
    const data = await response.json();
    showCategories(data.data);
-}
+};
 
 
 const sortByView = (target) => {
    target.classList.add('active')
    // target.active = true;
-   console.log(n);
    console.log(target);
-}
+};
+
 
--- JS/utilitis.js
+++ JS/utilitis.js
@@ -1,4 +1,4 @@
-
+const timeUndefinedElementId = [];
 // display categories
 const showCategories = (categoriesData) => {
 
@@ -62,26 +62,34 @@ const showCards = (cardsData) => {
          `;
       cardsContainer.appendChild(cardDiv);
    } else {
+      let count = 0;
       cardsData.forEach((card) => {
+         // console.log(card);
+         
+         const timeInHours = secondToHoursAndMinites(card?.others?.posted_date);
+         // console.log(timeInHours);
          const cardDiv = document.createElement('div');
          cardDiv.innerHTML =
             `
-         <div class="max-w-[350px] sm:max-w-[300px] md:max-w-[325px] lg:max-w-[310px]">
-         <figure class="mb-5 rounded-lg">
-            <img class="w-full rounded-lg" src="./thumbnil.png" alt="">
+         <div class="w-[350px] sm:w-[300px] md:w-[358px] lg:w-[317px] xl:w-[300px] 2xl:w-[360px]">
+         <figure class=" mb-5 rounded-lg relative">
+            <img class=" w-full h-[200px] rounded-lg" src="${card.thumbnail}" alt="">
+            <div id ="uploadTime${count}" class="  bg-[#171717] rounded text-white w-max absolute bottom-3 right-3">
+               <span  class=" uploaded-time ">${timeInHours ? timeInHours : ''}</span>
+            </div>
          </figure>
          <div class="flex gap-4">
-            <figure class="w-[25%]">
-               <img class="rounded-full " src="./olivia.jpg" alt="">
+            <figure class="w-[46px]  rounded-full">
+               <img class="rounded-full w-[46px] h-[46px] object-cover object-center  " src="${card?.authors[0]?.profile_picture}" alt="">
             </figure>
             <div class="">
-               <h3 class="font-bold leading-6 mb-3">Building a Winning UX Strategy Using the Kano Model</h3>
+               <h3 class="font-bold leading-6 mb-3">${card?.title}</h3>
                <div class="flex items-center gap-3 mb-1">
-                  <span class="text-sm">Awlad Hossain</span>
-                  <span><img src="./image/bluetik.png" alt=""></span>
+                  <span class="text-sm">${card?.authors[0]?.profile_name}</span>
+                  <span><img src="${card?.authors[0]?.verified ? './image/bluetik.png' : ''}" alt=""></span>
             </div>
             <div class="text-sm">
-               <span>91K</span>
+               <span>${card?.others?.views}</span>
                views
             </div>
             </div>
@@ -89,6 +97,7 @@ const showCards = (cardsData) => {
       </div>
          `;
          cardsContainer.appendChild(cardDiv);
+         count++;
       });
    }
 
@@ -105,3 +114,35 @@ const goHomePage = () => {
    console.log('blogPage');
    window.location.href = './index.html';
 }
+
+
+
+const secondToHoursAndMinites = (sec) => {
+   // console.log(sec);
+   const secondInNumber = Number(sec);
+   // console.log(secondInNumber);
+
+
+   const hours = Math.floor(secondInNumber / 3600);
+   const minutes = Math.floor((secondInNumber - (hours * 3600)) / 60);
+   // console.log(`${hours}hrs ${minutes}min`);
+   if (!secondInNumber) {
+      return ;
+   } else {
+      return `${hours}hrs ${minutes}min ago`;
+   }
+}
+
+const removeUploadTimeElement = (id) => {
+   console.log(id);
+   timeUndefinedElementId.push(id);
+   const showTimeElementConteiner = document.getElementById(id);
+   console.log(timeUndefinedElementId);
+   // showTimeelementConteiner.classList.add('hidden')
+
+
+}
+const timeUndefinedElement = () =>{
+   console.log(timeUndefinedElementId);
+}
+
